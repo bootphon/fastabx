213e334d9c2b913029233eeadd209691e3e7e9af
mxmpl <44207491+mxmpl@users.noreply.github.com>
2025-05-27 18:04:01 +0200

    subsampling refactor

--- src/fastabx/subsample.py
+++ src/fastabx/subsample.py
@@ -1,67 +1,107 @@
 """Subsampling functions."""
 
+import abc
+from collections.abc import Iterable
+
 import polars as pl
 import polars.selectors as cs
 
 from fastabx.verify import verify_subsampler_params
 
 
-def subsample_each_cell(df: pl.LazyFrame, size: int, seed: int) -> pl.LazyFrame:
-    """Subsample each cell by taking at most ``size`` instances of A, B, and X independently."""
-    return (
-        df.with_columns(pl.concat_str(~cs.starts_with("index"), separator="-").alias("__group"))
-        .with_columns(cs.starts_with("index").explode().shuffle(seed=seed).implode().over("__group").list.head(size))
-        .select(cs.exclude("__group"))
-    )
+class Subsampler(abc.ABC):
+    """Subsample the ABX :py:class:`.Task`."""
 
+    _description: str
 
-def subsample_across_group(df: pl.LazyFrame, size: int, seed: int) -> pl.LazyFrame:
-    """Subsample each group of 'across' condition by taking ``size`` possible values for X in each group."""
-    x_cols = [c for c in df.collect_schema() if c.endswith("_x") and c != "index_x"]
-    to_ignore = cs.starts_with("index") | cs.ends_with("_x")
-    df = df.with_columns(pl.concat_str(~to_ignore, separator="-").alias("__group"))
-    return (
-        df.group_by("__group", maintain_order=True)
-        .agg((cs.ends_with("_x") & (~cs.starts_with("index"))).unique(maintain_order=True).shuffle(seed).head(size))
-        .explode(x_cols)
-        .join(df, on=["__group", *x_cols], how="left")
-        .select(cs.exclude("__group"))
-    )
-
-
-class Subsampler:
-    """Subsample the ABX :py:class:`.Task`.
-
-    Each cell is limited to ``max_size_group`` items for A, B and X independently.
-    When using "across" conditions, each group of (A, B) is limited to ``max_x_across`` possible values for X.
-    Subsampling for one or more conditions can be disabled by setting the corresponding argument to ``None``.
-
-    :param max_size_group: Maximum number of instances of A, B, or X in each :py:class:`.Cell`.
-        Set to 10 in the original ZeroSpeech ABX code. Disabled if set to ``None``.
-    :param max_x_across: In the "across" speaker mode, maximum number of X considered for given values of A and B.
-        Set to 5 in the original ZeroSpeech ABX code. Disabled if set to ``None``.
-    :param seed: The random seed for the subsampling, default is 0.
-    """
-
-    def __init__(self, max_size_group: int | None, max_x_across: int | None, seed: int = 0) -> None:
-        verify_subsampler_params(max_size_group, max_x_across, seed=seed)
-        self.max_size_group = max_size_group
-        self.max_x_across = max_x_across
-        self.seed = seed
-
-    def __call__(self, lazy_cells: pl.LazyFrame, *, with_across: bool) -> pl.LazyFrame:
-        """Subsample the cells."""
-        if self.max_size_group is not None:
-            lazy_cells = subsample_each_cell(lazy_cells, self.max_size_group, self.seed)
-        if with_across and self.max_x_across is not None:
-            lazy_cells = subsample_across_group(lazy_cells, self.max_x_across, self.seed)
-        return lazy_cells
-
-    def description(self, *, with_across: bool) -> str:
+    def description(self) -> str:
         """Return a description of the subsampling."""
-        desc = []
-        if self.max_size_group is not None:
-            desc.append(f"maximal number of A, B, or X in a cell: {self.max_size_group}")
-        if with_across and self.max_x_across is not None:
-            desc.append(f"maximal number of X for (A, B): {self.max_x_across}")
-        return ",".join(desc)
+        return self._description
+
+    @abc.abstractmethod
+    def __call__(self, cells: pl.LazyFrame) -> pl.LazyFrame:
+        """Subsample the cells."""
+
+    def __add__(self, other: "Subsampler") -> "ConcatSubsampler":
+        """Chain multiple :py:class:`.Subsampler`."""
+        return ConcatSubsampler([self, other])
+
+    def __repr__(self) -> str:
+        return f"Subsampler({self.description()})"
+
+
+class ConcatSubsampler(Subsampler):
+    """Concatenation of multiple :py:class:`.Subsampler`. Apply them one after the other."""
+
+    def __init__(self, subsamplers: Iterable[Subsampler]) -> None:
+        self._subsamplers = list(subsamplers)
+        self._description = ", ".join([subsampler.description() for subsampler in self._subsamplers])
+
+    def __call__(self, cells: pl.LazyFrame) -> pl.LazyFrame:
+        """Subsample the cells."""
+        for subsampler in self._subsamplers:
+            cells = subsampler(cells)
+        return cells
+
+
+class SizeSubsampler(Subsampler):
+    """Subsample each cell by taking at most ``size`` instances of A, B, and X independently."""
+
+    def __init__(self, seed: int, size: int) -> None:
+        self._seed = seed
+        self._size = size
+        self._description = f"maximal number of A, B, or X in a cell: {self._size}"
+        verify_subsampler_params(size, seed=seed)
+
+    def __call__(self, cells: pl.LazyFrame) -> pl.LazyFrame:
+        """Subsample the cells."""
+        return (
+            cells.with_columns(pl.concat_str(~cs.starts_with("index"), separator="-").alias("__group"))
+            .with_columns(
+                cs.starts_with("index")
+                .explode()
+                .shuffle(seed=self._seed)
+                .implode()
+                .over("__group")
+                .list.head(self._size)
+            )
+            .select(cs.exclude("__group"))
+        )
+
+
+class AcrossGroupSubsampler(Subsampler):
+    """Subsample each group of 'across' condition by taking ``size`` possible values for X in each group."""
+
+    def __init__(self, seed: int, size: int) -> None:
+        self._seed = seed
+        self._size = size
+        self._description = f"maximal number of X for (A, B): {self._size}"
+        verify_subsampler_params(size, seed=seed)
+
+    def __call__(self, cells: pl.LazyFrame) -> pl.LazyFrame:
+        """Subsample the cells."""
+        x_cols = [c for c in cells.collect_schema() if c.endswith("_x") and c != "index_x"]
+        to_ignore = cs.starts_with("index") | cs.ends_with("_x")
+        cells = cells.with_columns(pl.concat_str(~to_ignore, separator="-").alias("__group"))
+        return (
+            cells.group_by("__group", maintain_order=True)
+            .agg((cs.ends_with("_x") & (~cs.starts_with("index"))).unique().shuffle(self._seed).head(self._size))
+            .explode(x_cols)
+            .join(cells, on=["__group", *x_cols], how="left")
+            .select(cs.exclude("__group"))
+        )
+
+
+def librilight_subsampler(seed: int, max_size_group: int | None, max_x_across: int | None) -> Subsampler | None:
+    """Replicates Libri-Light subsampling."""
+    match max_size_group, max_x_across:
+        case None, None:
+            return None
+        case int(), None:
+            return SizeSubsampler(seed, max_size_group)
+        case None, int():
+            return AcrossGroupSubsampler(seed, max_x_across)
+        case int(), int():
+            return SizeSubsampler(seed, max_size_group) + AcrossGroupSubsampler(seed, max_x_across)
+        case _:
+            raise ValueError(max_size_group, max_x_across)
