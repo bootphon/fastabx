622a5a05d10a548785dc4a4edf1a12fc4bb295c1
mxmpl <44207491+mxmpl@users.noreply.github.com>
2025-08-20 18:29:37 +0200

    stable

--- src/fastabx/__init__.py
+++ src/fastabx/__init__.py
@@ -1,9 +1,4 @@
-# ruff: noqa: E402
 """Full ABX."""
-
-from fastabx.utils import load_dtw_extension
-
-load_dtw_extension()
 
 from fastabx.dataset import Dataset
 from fastabx.pooling import pooling
@@ -11,5 +6,7 @@ from fastabx.score import Score
 from fastabx.subsample import Subsampler
 from fastabx.task import Task
 from fastabx.zerospeech import zerospeech_abx
+
+from . import _C  # type: ignore[attr-defined] # noqa: F401
 
 __all__ = ["Dataset", "Score", "Subsampler", "Task", "pooling", "zerospeech_abx"]
