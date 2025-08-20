bdab53573b59a9ea760be6fceedcf40887d306cc
mxmpl <44207491+mxmpl@users.noreply.github.com>
2025-08-20 18:56:54 +0200

    update

--- src/fastabx/__init__.py
+++ src/fastabx/__init__.py
@@ -1,4 +1,8 @@
+# ruff: noqa: F401, I001
 """Full ABX."""
+
+import torch
+from . import _C  # type: ignore[attr-defined]
 
 from fastabx.dataset import Dataset
 from fastabx.pooling import pooling
@@ -7,6 +11,5 @@ from fastabx.subsample import Subsampler
 from fastabx.task import Task
 from fastabx.zerospeech import zerospeech_abx
 
-from . import _C  # type: ignore[attr-defined] # noqa: F401
 
 __all__ = ["Dataset", "Score", "Subsampler", "Task", "pooling", "zerospeech_abx"]
