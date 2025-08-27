f6cd17fe43435678f2dad2052597a369b6796195
mxmpl <44207491+mxmpl@users.noreply.github.com>
2025-08-27 16:10:28 +0200

    update

--- src/fastabx/utils.py
+++ src/fastabx/utils.py
@@ -5,16 +5,6 @@ import os
 MIN_CELLS_FOR_TQDM = 50
 
 
-def load_dtw_extension() -> None:
-    """Load the DTW extension.
-
-    We check that PyTorch has been installed with the correct version.
-    """
-    import torch  # noqa: F401, PLC0415
-
-    from . import _C  # ty: ignore[unresolved-import] # noqa: F401, PLC0415
-
-
 def with_librilight_bug() -> bool:
     """Whether to reproduce the results from LibriLight ABX or not."""
     return os.getenv("FASTABX_WITH_LIBRILIGHT_BUG", "0") == "1"
